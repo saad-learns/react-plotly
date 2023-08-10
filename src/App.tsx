9ad6c26e5890e55ffd3041f34dc1e2cf988533e2
Mia, Mashrur (DI PA SW DH APM 4 1) <mashrur.mia@siemens.com>
2023-08-10 10:52:11 +0100

    Add a plotly graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Plot, { Figure, PlotParams } from "react-plotly.js";
+import { Data } from "plotly.js";
 
 function App() {
+  const sampleData: Data[] = [
+    {
+      x: [-3, -2, -1, 0, 1, 2, 3],
+      y: [
+        -0.1411200080598672, -0.9092974268256817, -0.8414709848078965, 0,
+        0.8414709848078965, 0.9092974268256817, 0.1411200080598672,
+      ],
+    },
+  ];
+  const [plotParams, setPlotParams] = React.useState<PlotParams>({
+    data: sampleData,
+    layout: {},
+  });
+
+  const updateData = () => {
+    setPlotParams(() => ({
+      data: JSON.parse(JSON.stringify(sampleData)),
+      layout: {},
+    }));
+  };
+
+  const onInitialized = (figure: Readonly<Figure>) =>
+    console.log("initialized with:", figure);
+
+  const onUpdate = (figure: Readonly<Figure>) =>
+    console.log("updated as: ", figure);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ padding: 10 }}>
+        <button onClick={updateData}>Update Data</button>
+      </div>
+
+      <Plot
+        data={plotParams.data}
+        layout={plotParams.layout}
+        onInitialized={onInitialized}
+        onUpdate={onUpdate}
+      />
     </div>
   );
 }
